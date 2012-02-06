57108a110ab1ac0e435f1fe6764c9e7e2d927735
Lee Gentry <leegentry@leebook.local>
2012-02-06 14:09:19 -0500

    herp derp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baconnearme::Application.config.secret_token = '9ce95a6929848a3caec45e2bee1c6ccb101f12c5b223a2950b04e5a18d91f6c2fd5d5b0b5660a989c02fad4a7d44e779bfb9051d7d46248fa29ac0bc5552b359'
